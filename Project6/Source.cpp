0c0a5f9889bb854f6a7dc35f0aaafdf5098c9e46
Filippp554 <91588605+Filippp554@users.noreply.github.com>
2022-01-12 08:54:07 +0100

    Add files via upload

---
+++ Project6/Source.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int klop = 0;
+	int Penny = 3;
+
+	do
+	{
+		cout << "Klop!" << endl;
+		klop++;
+	} while (klop < Penny);
+	cout << "Penny!" << endl;
+}
